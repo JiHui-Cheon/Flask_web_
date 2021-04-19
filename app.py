8d023890d3c415e0f90699c4e49ad6ac550d6ec8
cjh <cjh@example.com>
2021-04-19 16:01:51 +0900

    리스트 처리 articles.html for문

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template
+from data import Articles
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods=['GET'])
+def index():
+    # return "Hello World!"
+    return render_template("index.html") #첫번째 인자로 파일 경로를 받음. 두번째 인자로 전달할 데이터를 받는다.
+
+@app.route('/about')
+def about():
+    return render_template("about.html", hello = "Gary Kim") 
+    
+@app.route('/articles')
+def articles():
+    articles = Articles()
+    # print(articles[0]['title']) 콘솔창에 잘 뜨는지 확인
+    return render_template("articles.html", articles = articles) 
+
+
+
+if __name__ == '__main__': # 처음 서버 띄울때 쓰임.
+    app.run()
